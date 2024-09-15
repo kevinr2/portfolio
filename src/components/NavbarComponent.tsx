b0e20480f0f313dce2f13ce1efc2d871483f4256
kevinr2 <kevinreve096@gmail.com>
2024-09-15 17:25:58 -0500

    index design

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,8 +1,13 @@
 "use client"
 import Link from "next/link";
-import { Bowlby_One} from 'next/font/google'
+import { Bowlby_One,Montserrat} from 'next/font/google'
 
 const bowls =Bowlby_One({
+  weight:'400',
+  subsets:["latin"],
+  display:'swap'
+})
+const monserrat =Montserrat({
   weight:'400',
   subsets:["latin"],
   display:'swap'
@@ -11,12 +16,11 @@ const bowls =Bowlby_One({
 
 export const NavbarComponent = () => {
   return (
-    <div className="fixed  text-n w-full  mt-6 h-14 flex justify-evenly items-center">
+    <div className="fixed  text-n w-full px-4 mt-3 h-14 flex justify-between items-center">
       <Link href="/">
-      <div className="triangulo">
-      </div>
+        <h1 className={bowls.className}>Kevin<strong>Rev</strong></h1>
       </Link>
-      <div className={`${bowls.className} flex gap-5 text-lg justify-center`}> 
+      <div className={`${monserrat.className} flex gap-5 text-lg justify-center`}> 
         
 
         <Link href="/about">About</Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,21 @@
 import Link from "next/link";
 import Inner from "./inner/Inner";
 
+
 export default function Home() {
   return (
     <>
       <Inner>
-        <div className="w-[100%] h-[100vh]  md:flex justify-around items-center">
-          <div className="w-full sm:w-1/3  lg:w-1/3 xl:w-1/3   ml-10">
-            <h1 className="text-5xl   font-extrabold tracking-tight">
-            ¡Hola! Soy Kevin 
+        <div className="w-[100%] h-[100vh] px-[20%] flex  items-center">
+          <div className="mt-5">
+            <h1 className=" min-[319px]:text-5xl sm:text-5xl md:text-5xl lg:text-7xl  font-extrabold tracking-tight">
+              Kevin Revelo
+              Desarrollador frontend apasionado
+              Creativo e innovador. 
             </h1>
-            <p>
-               Desarrollador frontend apasionado. Trabajo con
-              React, Next.js y React Native para crear aplicaciones web y
-              móviles rápidas, seguras y optimizadas. Me encanta diseñar
-              experiencias que sean tanto atractivas como fáciles de usar. Si
-              buscas una solución digital que destaque.
-            </p>
-            <Link
-              href="/about"
-              className="inline-flex mt-5 items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-black dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              ¡Estoy aquí para ayudarte!
-            </Link>
-          </div>
-          <div className="w-1/2 h-1/5 flex justify-center items-center ">
-            <h1>Soy Kevin R</h1>
+            <p></p>
+            <Link href="/about" className="btn "><button className="button mt-3" >Cv Kevin</button> </Link>
+            
           </div>
         </div>
       </Inner>
