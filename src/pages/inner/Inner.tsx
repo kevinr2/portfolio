a0bc4e7a43f372e632044eb91589defe8ad8ab07
kevinr2 <kevinreve096@gmail.com>
2024-09-14 23:16:14 -0500

    organizacon

--- src/pages/inner/Inner.tsx
+++ src/pages/inner/Inner.tsx
@@ -1,84 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import { motion, Variants } from 'framer-motion';
+import { motion, Variants } from "framer-motion";
 
-import { opacity, expand,text } from '../../anim/anim';
-import { NavbarComponent } from '@/components/NavbarComponent';
-import { Rubik_Glitch_Pop } from 'next/font/google'
- 
+import { opacity, expand, text } from "../../anim/anim";
+import { NavbarComponent } from "@/components/NavbarComponent";
+import { Rubik_Glitch_Pop } from "next/font/google";
+
 const rubik = Rubik_Glitch_Pop({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const anim = (variants: Variants, custom?: number) => {
+    return {
+      initial: "initial",
 
+      animate: "enter",
 
-export default function Layout({children}:{children:React.ReactNode}) {
+      exit: "exit",
 
+      custom,
 
+      variants,
+    };
+  };
 
-    const anim = (variants:Variants, custom?:number) => {
+  const nombre = "KEVIN";
+  const letrasNombre = nombre.split("");
 
-        return {
+  return (
+    <div className="page stairs" style={{ backgroundColor: "white" }}>
+      <motion.div {...anim(opacity)} className="transition-background" />
 
-            initial: "initial",
+      <div className="transition-container">
+        {letrasNombre.map((letra, i) => {
+          return (
+            <motion.div key={i} {...anim(expand, letrasNombre.length - i)}>
+              <motion.h1 {...anim(text)} className={rubik.className}>
+                {letra}
+              </motion.h1>
+            </motion.div>
+          );
+        })}
+      </div>
+      <NavbarComponent />
 
-            animate: "enter",
-
-            exit: "exit",
-
-            custom,
-
-            variants
-
-        }
-
-    }
-
-
-
-    const nombre = "KEVIN"; 
-    const letrasNombre = nombre.split(""); 
-
-    return (
-
-        <div className='page stairs' style={{backgroundColor:'white'}}>
-
-            <motion.div {...anim(opacity)} className='transition-background'/>
-
-            <div className='transition-container'>
-
-                {
-
-                    letrasNombre.map( (letra, i) => {
-
-                        return (
-
-                            <motion.div key={i} {...anim(expand, letrasNombre.length - i)}>
-                               
-                                    <motion.h1 {...anim(text)} className={rubik.className} >{letra}</motion.h1>
-                                
-                            
-                                
-                            </motion.div>
-
-                        ) 
-
-                    })
-
-                }
-
-            </div>
-            <NavbarComponent/>
-
-            {
-
-                children
-
-            }
-
-        </div>
-
-    )
-
+      {children}
+    </div>
+  );
 }
