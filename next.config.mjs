e023db043fd3aee126d47b31280b06a9bd5fff3b
kevinr2 <kevinreve096@gmail.com>
2024-09-16 19:38:09 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      // Si usas subdirectorios o GitHub Pages en una carpeta
+  assetPrefix: 'https://kevinrevelo.com/',
+
+  // Si usas algún prefijo en las rutas
+  basePath: '',
+
+  // Otros ajustes de Next.js
   reactStrictMode: true,
 };
 
